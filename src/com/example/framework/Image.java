c73ab7e537fd67d3fec17eabef83351da612b7c2
boerema <adamboerema@gmail.com>
2014-02-09 03:22:23 -0500

    updated

---
+++ src/com/example/framework/Image.java
@@ -0,0 +1,13 @@
+package com.example.framework;
+
+import com.example.framework.Graphics.ImageFormat;
+
+public interface Image {
+	public int getWidth();
+	
+	public int getHeight();
+	
+	public ImageFormat getFormat();
+	
+	public void dispose();
+}
---
+++ src/com/example/framework/Music.java
@@ -0,0 +1,5 @@
+package com.example.framework;
+
+public interface Music {
+
+}
