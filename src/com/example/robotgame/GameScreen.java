6fb6d6a05fff51455a0e7bc0e3e9cffbd6e4b0db
boerema <adamboerema@gmail.com>
2014-03-18 00:58:21 -0400

    started implementing the game using the framework

---
+++ src/com/example/robotgame/GameScreen.java
@@ -0,0 +1,22 @@
+package com.example.robotgame;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import com.example.framework.Game;
+import com.example.framework.Graphics;
+import com.example.framework.Image;
+import com.example.framework.Input.TouchEvent;
+import com.example.framework.Screen;
+
+public class GameScreen {
+
+	public GameScreen() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
